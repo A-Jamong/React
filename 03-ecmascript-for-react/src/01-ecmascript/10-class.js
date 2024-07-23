e2fee138b5bd26d1d3641434208200446832463b
JAM <99.1.some@gmail.com>
2024-07-23 15:25:45 +0900

    [ 실습 ] 모듈 내보내고 가져오기 실습해봄

--- 03-ecmascript-for-react/src/01-ecmascript/10-class.js
+++ 03-ecmascript-for-react/src/01-ecmascript/10-class.js
@@ -1,48 +1,14 @@
 // --------------------------------------------------------------------------
 // ES5 Constructor (like. Class) with Module Pattern (IIFE)
 
-const _ApiClient = (function () {
-  function ApiClient(endpoint) {
-    this._endpoint = endpoint;
-  }
 
-  ApiClient.prototype.readAll = function (page = 1, pageSize = 5) {
-    return fetch(`${this._endpoint}?_page=${page}&_limit=${pageSize}`).then(
-      (response) => response.json()
-    );
-  };
-
-  ApiClient.prototype.readOne = function (id) {
-    return fetch(`${this._endpoint}/${id}`).then((response) => response.json());
-  };
-
-  ApiClient.prototype.create = function (data) {
-    return fetch(this._endpoint, {
-      method: 'POST',
-      body: JSON.stringify(data),
-    }).then((response) => response.json());
-  };
-
-  ApiClient.prototype.update = function (id, data) {
-    return fetch(`${this._endpoint}/${id}`, {
-      method: 'PATCH',
-      body: JSON.stringify(data),
-    }).then((response) => response.json());
-  };
-
-  ApiClient.prototype.delete = function (id) {
-    return fetch(`${this._endpoint}/${id}`, {
-      method: 'DELETE',
-    }).then((response) => response.json());
-  };
-
-  return ApiClient;
-})();
 
 // 🔶 Class 구문을 사용해 ApiClient 클래스를 작성합니다.
 // 참고: https://mzl.la/3UdwrNE
 
-const todosService = new _ApiClient(
+import { _ApiClient as AC } from "./api/ApiClient";
+
+const todosService = new AC(
   'https://jsonplaceholder.typicode.com/todos'
 );
 

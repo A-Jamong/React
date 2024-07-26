51127562f70e0fd2279d2351ce1d664f9960b1be
JAM <99.1.some@gmail.com>
2024-07-26 15:01:42 +0900

    [ 실습 ] 드디어마참내 뭔가 오류를 해결하고 화면에 렌더링되는 거 구경중

--- 05-react-component-with-props/server.mjs
+++ 05-react-component-with-props/server.mjs
@@ -1,11 +1,12 @@
-import liveServer from "live-server";
+// JavaScript를 사용해 Node.js 런타임 환경에서 실행시킬 프로그램 작성
+
+import liveServer from 'live-server';
 
 const params = {
-  host: "localhost",
+  host: 'localhost',
   port: 3000,
-  root: ".",
   open: false,
-  mount: [["/", "./public"]],
+  mount: [['/', './public']],
 };
 
 liveServer.start(params);
---
+++ 05-react-component-with-props/src/components/architectures/ArchitectureList.function.js
@@ -0,0 +1,13 @@
+import React from 'https://esm.sh/react'
+function ArchitectureList(props) {
+  // 컴포넌트 속성 확인
+  // console.log(props);
+  const { lang, children } = props;
+
+  // 리액트 엘리먼트(React Element) 반환
+  return React.createElement(
+    'ul',
+    { className: 'architectures', lang },
+    children
+  );
+}

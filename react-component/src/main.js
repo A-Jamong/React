a22e9d92a6d75a94a87619f7a7f714e77f76750d
JAM <99.1.some@gmail.com>
2024-07-28 18:02:24 +0900

    [ 과제 ] 1. html/css 로 만들어보기
    - default 상태일 때 chip--default
    - check 상태일 때 chip--check
    - disabled 상태일 때 chip--disabled
    + check 상태일 때 이미지가 나타나게 해야되는데!!

---
+++ react-component/src/main.js
@@ -0,0 +1,13 @@
+import React, { createElement as cE } from "https://esm.sh/react";
+import { createRoot } from 'https://esm.sh/react-dom';
+
+const container = document.getElementById('root');
+const reactDomRoot = createRoot(container);
+
+
+
+function chips(){
+  return cE(
+    ''
+  )
+}

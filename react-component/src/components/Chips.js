556da4e3e6e961ff6be328ac457a564f7d44bde9
JAM <99.1.some@gmail.com>
2024-07-28 18:48:49 +0900

    [ 제거 ] 쓰지 않은 변수 주석처리

--- react-component/src/components/Chips.js
+++ react-component/src/components/Chips.js
@@ -22,7 +22,7 @@ import { createRoot } from "https://esm.sh/react-dom";
     </div>
 */
 
-// img 는 선택되었을 때만 나타나게 하고 싶은데 어떻게 해야될지 고민.
+// img 는 선택되었을 때만 나타나게 하고 싶은데 어떻게 해야될지 고민. -> && 을 통해서 status가 select일 경우 이미지 추가하게 했다! 
 
 export default function chips({
   chipName = "single-chip",
@@ -30,8 +30,8 @@ export default function chips({
 }) {
   // aria-lable 용! chipName + status에 따라 선택할 수 있습니다/선택했습니다/선택할 수 없습니다.
   let statusMessage = "";
-  // 상태에 따라서 모양새가 바뀌니까 statusCss 로 클래스명을 줘서 추가/삭제하는 식
-  let statusCss = "";
+  // 상태에 따라서 모양새가 바뀌니까 statusCss 로 클래스명을 줘서 추가/삭제하는 식 -> 상태에 따른 css를 --default, --select로 해놔서 status값만 있으면 클래스 이름을 줄 수 있어서 해결됐다!
+  // let statusCss = "";
 
   switch (status) {
     default:

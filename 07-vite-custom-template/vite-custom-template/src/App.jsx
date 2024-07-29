00983aaaaeae594b64006ca43fd1e0832e744681
JAM <99.1.some@gmail.com>
2024-07-29 13:37:05 +0900

    [ 실습 ] 커스텀 비트 만들기 3
    - vite flugin
    - vite config

---
+++ 07-vite-custom-template/vite-custom-template/src/App.jsx
@@ -0,0 +1,17 @@
+// React is not defined
+// import Reaact from 'react'
+// import { jsx } from 'react/jsx-runtime';
+
+// JSX Runtime : Classic
+// JSX Runtime : Automatic, new JSX Transform (React v17+)
+// current React Version : v18, v19 RC
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
+
+export default App;
--- 07-vite-custom-template/vite-custom-template/src/main.jsx
+++ 07-vite-custom-template/vite-custom-template/src/main.jsx
@@ -1,13 +1,10 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React 웹 앱</h1>
-    </div>
-  );
-}
+// vite 클라이언트 환경에서 사용가능한 환경 변수
+// BASE_URL : '/' 은 퍼블릭 폴더 가리킴
+console.log(import.meta.env);
 
 const domN = document.getElementById("react-app");
 ReactDOM.createRoot(domN).render(
